ca63f6746adb00ad68ea2b0237940c42ecdd248e
何康 <hekang19921111@hotmail.com>
2025-06-16 17:59:33 +0800

    add:新增首页2

--- credibot/home/HomeChildView.swift
+++ credibot/home/HomeChildView.swift
@@ -23,6 +23,59 @@ class HomeChildView: BaseView {
         return headImageView
     }()
     
+    lazy var speakerImageView: UIImageView = {
+        let speakerImageView = UIImageView()
+        speakerImageView.image = UIImage(named: "speckimge")
+        return speakerImageView
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.text = desc + " " + pro
+        nameLabel.textColor = .black
+        nameLabel.textAlignment = .left
+        nameLabel.font = UIFont.systemFont(ofSize: 18.pix(), weight: .bold)
+        return nameLabel
+    }()
+    
+    lazy var descLabel: UILabel = {
+        let descLabel = UILabel()
+        descLabel.text = "Go to Apply"
+        descLabel.textColor = .init(colorHex: "#999999")
+        descLabel.textAlignment = .right
+        descLabel.font = UIFont.systemFont(ofSize: 12.pix(), weight: .regular)
+        return descLabel
+    }()
+    
+    lazy var desc1Label: UILabel = {
+        let desc1Label = UILabel()
+        desc1Label.textColor = .white
+        desc1Label.textAlignment = .left
+        desc1Label.font = UIFont.systemFont(ofSize: 14.pix(), weight: .semibold)
+        return desc1Label
+    }()
+    
+    lazy var moneyLabel: UILabel = {
+        let moneyLabel = UILabel()
+        moneyLabel.textColor = .white
+        moneyLabel.textAlignment = .left
+        moneyLabel.font = UIFont.systemFont(ofSize: 44.pix(), weight: .semibold)
+        return moneyLabel
+    }()
+    
+    lazy var rateLabel: UILabel = {
+        let rateLabel = UILabel()
+        rateLabel.textColor = .white
+        rateLabel.textAlignment = .center
+        rateLabel.backgroundColor = .clear
+        rateLabel.layer.cornerRadius = 11.pix()
+        rateLabel.layer.masksToBounds = true
+        rateLabel.layer.borderWidth = 0.5.pix()
+        rateLabel.layer.borderColor = UIColor.white.cgColor
+        rateLabel.font = UIFont.systemFont(ofSize: 12.pix(), weight: .regular)
+        return rateLabel
+    }()
+    
     lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
         tableView.separatorStyle = .none
@@ -37,6 +90,11 @@ class HomeChildView: BaseView {
             tableView.sectionHeaderTopPadding = 0
         }
         return tableView
+    }()
+    
+    lazy var mcView: RouteOrderTypeView = {
+        let mcView = RouteOrderTypeView()
+        return mcView
     }()
     
     lazy var marqueeView: MarqueeView = {
@@ -94,8 +152,6 @@ extension HomeChildView: UITableViewDelegate {
             make.size.equalTo(CGSize(width: 345.pix(), height: 195.pix()))
         }
         
-        let speakerImageView = UIImageView()
-        speakerImageView.image = UIImage(named: "speckimge")
         headImageView.addSubview(speakerImageView)
         speakerImageView.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(23.5.pix())
@@ -103,24 +159,12 @@ extension HomeChildView: UITableViewDelegate {
             make.size.equalTo(CGSize(width: 18.pix(), height: 18.pix()))
         }
         
-        let nameLabel = UILabel()
-        nameLabel.text = desc + " " + pro
-        nameLabel.textColor = .black
-        nameLabel.textAlignment = .left
-        nameLabel.font = UIFont.systemFont(ofSize: 18.pix(), weight: .bold)
-        
         headView.addSubview(nameLabel)
         nameLabel.snp.makeConstraints { make in
             make.left.equalToSuperview().offset(15.pix())
             make.bottom.equalToSuperview().offset(-4.pix())
             make.height.equalTo(20.pix())
         }
-        
-        let descLabel = UILabel()
-        descLabel.text = "Go to Apply"
-        descLabel.textColor = .init(colorHex: "#999999")
-        descLabel.textAlignment = .right
-        descLabel.font = UIFont.systemFont(ofSize: 12.pix(), weight: .regular)
         
         headView.addSubview(descLabel)
         descLabel.snp.makeConstraints { make in
@@ -142,6 +186,77 @@ extension HomeChildView: UITableViewDelegate {
         marqueeView.onItemTapped = { [weak self] item in
             self?.pageBlock?(item.admiration ?? "")
         }
+        
+        
+        headImageView.addSubview(desc1Label)
+        desc1Label.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(30.pix())
+            make.top.equalToSuperview().offset(65.pix())
+            make.height.equalTo(14.pix())
+        }
+        
+        
+        headImageView.addSubview(moneyLabel)
+        moneyLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(30.pix())
+            make.top.equalTo(desc1Label.snp.bottom).offset(15.pix())
+            make.height.equalTo(44.pix())
+        }
+        
+        let iconImagView = UIImageView()
+        iconImagView.layer.cornerRadius = 4.pix()
+        iconImagView.layer.masksToBounds = true
+        
+        let productName = UILabel()
+        productName.textColor = .black
+        productName.textAlignment = .center
+        productName.font = UIFont.systemFont(ofSize: 12.pix(), weight: .semibold)
+        
+        headImageView.addSubview(productName)
+        productName.snp.makeConstraints { make in
+            make.right.equalToSuperview().offset(-31.pix())
+            make.height.equalTo(23.pix())
+            make.bottom.equalTo(moneyLabel.snp.bottom).offset(-6.pix())
+        }
+        
+        headImageView.addSubview(iconImagView)
+        iconImagView.snp.makeConstraints { make in
+            make.right.equalTo(productName.snp.left).offset(-4.pix())
+            make.centerY.equalTo(productName.snp.centerY)
+            make.size.equalTo(CGSize(width: 23.pix(), height: 23.pix()))
+        }
+        
+        headImageView.addSubview(rateLabel)
+        rateLabel.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: 83.pix(), height: 25.pix()))
+            make.centerY.equalTo(desc1Label.snp.centerY)
+            make.right.equalToSuperview().offset(-30.pix())
+        }
+        
+        
+        headImageView.addSubview(mcView)
+        mcView.snp.makeConstraints { make in
+            make.height.equalTo(50.pix())
+            make.bottom.equalToSuperview()
+            make.width.equalTo(345.pix())
+            make.top.equalTo(moneyLabel.snp.bottom).offset(7.pix())
+        }
+        
+        if let model = self.floatModel.value?.lost?.skinny?[0] {
+            desc1Label.text = model.orbreaking ?? ""
+            moneyLabel.text = "₱\(model.clapping ?? "")"
+            productName.text = model.turnedquickly ?? ""
+            iconImagView.kf.setImage(with: URL(string: model.shorts ?? ""), placeholder: UIImage(named: "logoplaceh"))
+            rateLabel.text = model.impossible ?? ""
+            mcView.titleLabel.text = model.sliding ?? ""
+        }
+        
+        headImageView.rx.tapGesture().when(.recognized).subscribe(onNext: { [weak self] _ in
+            guard let self = self else { return }
+            if let model = self.floatModel.value?.lost?.skinny?[0] {
+                self.productBlock?(String(model.grabbed ?? 0))
+            }
+        }).disposed(by: disposeBag)
         
         return headView
     }
